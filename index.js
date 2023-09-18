588be45037b90a0a8bf90739921d0fb3b62b3b9a
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-09-18 19:50:04 -0300

    oi

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ let cam = document.querySelector('#oi');
 let cena = document.querySelector('#papai');
 let title = document.querySelector('#bah');
 
-cena.removeChild(cam)
-function bucejo(){
-cena.appendChild(cam)
-title.classList.remove('hidden')
-}
+
+const startARButton = document.getElementById('startARButton');
+const hideARButton = document.getElementById('hideARButton');
+const camera = document.querySelector('a-camera');
+
+startARButton.addEventListener('click', () => {
+ // Esconder o botão de iniciar
+ startARButton.style.display = 'none';
+ // Mostrar o botão de esconder
+ hideARButton.style.display = 'block';
+ // Ativar a câmera
+ camera.setAttribute('look-controls', 'enabled', true);
+});
+
+hideARButton.addEventListener('click', () => {
+ // Mostrar o botão de iniciar
+ startARButton.style.display = 'block';
+ // Esconder o botão de esconder
+ hideARButton.style.display = 'none';
+ // Desativar a câmera
+ camera.setAttribute('look-controls', 'enabled', false);
+});
