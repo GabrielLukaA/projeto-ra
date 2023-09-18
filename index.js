0838cb22a772efcc82a7034a1eb1640a6d3f57ad
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-09-18 20:18:17 -0300

    oi

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-let cam = document.querySelector('#oi');
-let cena = document.querySelector('#papai');
-let title = document.querySelector('#bah');
+// let cam = document.querySelector('#oi');
+// let cena = document.querySelector('#papai');
+// let title = document.querySelector('#bah');
 
 
-const startARButton = document.getElementById('startARButton');
-const hideARButton = document.getElementById('hideARButton');
-const camera = document.querySelector('a-scene');
-camera.setAttribute('look-controls', 'enabled', false);
-camera.classList.add("hidden")
+// const startARButton = document.getElementById('startARButton');
+// const hideARButton = document.getElementById('hideARButton');
+// const camera = document.querySelector('a-scene');
+// camera.setAttribute('look-controls', 'enabled', false);
+// camera.classList.add("hidden")
 
-startARButton.addEventListener('click', () => {
- // Esconder o botão de iniciar
- startARButton.style.display = 'none';
- // Mostrar o botão de esconder
- hideARButton.style.display = 'block';
- // Ativar a câmera
- camera.setAttribute('look-controls', 'enabled', true);
- camera.classList.remove("hidden")
-});
+// startARButton.addEventListener('click', () => {
+//  // Esconder o botão de iniciar
+//  startARButton.style.display = 'none';
+//  // Mostrar o botão de esconder
+//  hideARButton.style.display = 'block';
+//  // Ativar a câmera
+//  camera.setAttribute('look-controls', 'enabled', true);
+//  camera.classList.remove("hidden")
+// });
 
-hideARButton.addEventListener('click', () => {
- // Mostrar o botão de iniciar
- startARButton.style.display = 'block';
- // Esconder o botão de esconder
- hideARButton.style.display = 'none';
- // Desativar a câmera
- camera.setAttribute('look-controls', 'enabled', false);
- camera.classList.add("hidden")
-});
+// hideARButton.addEventListener('click', () => {
+//  // Mostrar o botão de iniciar
+//  startARButton.style.display = 'block';
+//  // Esconder o botão de esconder
+//  hideARButton.style.display = 'none';
+//  // Desativar a câmera
+//  camera.setAttribute('look-controls', 'enabled', false);
+//  camera.classList.add("hidden")
+// });
