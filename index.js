75bbdf3a4cef6562d0cd2e4fb364aded7de6b948
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-09-18 19:39:12 -0300

    oi

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let cam = document.querySelector('#oi');
+let cena = document.querySelector('#papai');
 let title = document.querySelector('#bah');
 
-
+cena.removeChild(cam)
 function bucejo(){
-cam.classList.remove('hidden')
+cena.appendChild(cam)
 title.classList.remove('hidden')
 }
