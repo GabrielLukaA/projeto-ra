66b68d65bd2e71f706528ca9e47cd023ec5e49ba
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2023-09-25 21:34:53 -0300

    oie

---
+++ card.js
@@ -0,0 +1,3 @@
+let texto = document.querySelector("#texto")
+
+alert(texto)
--- index.js
+++ index.js
@@ -8,32 +8,9 @@ function direcionarCamera(){
     window.location.href = 'camera.html';
 }
 
-function alterarPagina(){
-    window.location.href =  'card.html';
-    return true;
-}
 
 exampleTarget.addEventListener("targetFound", async event => {
-    if(alterarPagina()){
-        alert(equipamentos[0])
-    let campoTexto = document.getElementById('texto')
-
-    alert(campoTexto)
-
-    // let porra = document.createElement('p')
-    // let textin = document.createTextNode(equipamentos[0])
-    // porra.appendChild(textin)
-    // alert(porra)
-    // alert(porra.childNodes)
-
-    //  porra.innerText = equipamentos[0]
-
-    // alert(porra.innerText)
-
-    // document.appendChild(porra)
-    // document.getElementById('body').appendChild(porra)
-    }
-   
+    window.location.href =  'card.html';
 });
 
   //cpa que fazemos na propria camera.html mesmo e ver se da
