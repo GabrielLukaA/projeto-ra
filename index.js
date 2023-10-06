f37748c937e775b443cb395bda038e3a194d2730
Gabriel Luka de Amaral <gabrielluka@weg.net>
2023-10-06 18:44:04 -0300

    oi

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-let exampleTarget = document.querySelector('#targetImage');
+// let exampleTarget = document.querySelector('#targetImage');
 
-let  equipamentos = [];
-equipamentos.push('Betoneira')
-
-
-function direcionarCamera(){
-    window.location.href = 'camera.html';
-}
+// let  equipamentos = [];
+// equipamentos.push('Betoneira')
 
 
-exampleTarget.addEventListener("targetFound", async event => {
-    window.location.href =  'card.html';
-    //mandar mensagem ou indice para o local storage e recuperar no outro js
+// function direcionarCamera(){
+//     window.location.href = 'camera.html';
+// }
 
-});
+
+// exampleTarget.addEventListener("targetFound", async event => {
+//     window.location.href =  'card.html';
+//     //mandar mensagem ou indice para o local storage e recuperar no outro js
+
+// });
 
   //cpa que fazemos na propria camera.html mesmo e ver se da
     //q nada irmao samo bomd+
