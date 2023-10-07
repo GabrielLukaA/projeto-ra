7ea7a51689f0259dc282ae71dfce28d0c7dbcfbf
unknown <alebatistathiago@gmail.com>
2023-10-07 16:06:36 -0300

    teste

--- card.js
+++ card.js
@@ -14,3 +14,112 @@ function direcionarCamera(){
 }
 
 // alert(text.innerText)
+
+
+//maquina de frente
+// 18, 33
+
+//painel controle
+// 49, 49(2), 57, 17
+
+//garra de insercao
+// 19, 50
+
+//prendedor(?)/ trava seguranca
+// 51, 51(2)
+
+//prendedor(?)/ trava seguranca (lado do painel)
+// 1, 1(2), 69, 12
+
+//perigo alta tesão
+// 3
+
+//sensor f1
+//20
+
+//sensor f2
+// 4
+
+//protecao/parte de cima da garra de insercao
+// 5
+
+//visao lateral (esquerda) (colocadora de fitas)
+// 6, 6(2), 70, 38, 46(escrita/especificacoes), 32
+
+//valvula e cabos/motores(?)
+// 8, 24, 14, 42
+
+//maquina por tras
+// 9
+
+//motor
+// 34, 22, 52, 59, 41
+
+//sensor f7/visao superior
+// 10, 28
+
+//visao superior
+// 35, 27, 29
+
+//visao lateral (direita)/alta tensao
+// 11, 21
+
+//suporte (motor/valvula) (lateral)
+//23
+
+//sensor braco direito
+//36
+
+//prendedor(diferente)
+//37
+
+//visao interior (colocadora de fita)
+//39, 43, 44, 30, 45
+
+//cabo motor
+//71, 25
+
+//filtro hidraulico
+// 72, 13
+
+//suporte (visao superior)
+//40, 60, 16, 66    
+
+//insertadora
+//53
+
+//rosca da insertadora
+//54, 55
+
+//painel detector de falha de isolacao
+// 56
+
+//fita
+//26, 31
+
+//placa motor
+//58
+
+//conjunto painel controle/detector falha
+//61
+
+//conjunto lateral esquerda
+// 47
+
+//conjunto lateral direita
+//62
+
+//visao frontal (com insertadora)
+//63
+
+//bobinas
+//0, 64
+
+//cobre
+// 15
+
+//placa fundadores
+//65
+
+//conjunto lateral esquerda
+// 48
