90843a8e20e7b3bf6abc6c6c83cad0fc886788c1
unknown <alebatistathiago@gmail.com>
2023-10-07 11:53:16 -0300

    teste

--- index.js
+++ index.js
@@ -1,6 +1,77 @@
-let exampleTarget = document.querySelector('#targetImage0');
-let exampleTarget72 = document.querySelector('#targetImage72');
-let exampleTarget73 = document.querySelector('#targetImage73');
+let targetImage0  = document.querySelector('#targetImage0' )
+let targetImage1  = document.querySelector('#targetImage1' )
+let targetImage2  = document.querySelector('#targetImage2' )
+let targetImage3  = document.querySelector('#targetImage3' )
+let targetImage4  = document.querySelector('#targetImage4' )
+let targetImage5  = document.querySelector('#targetImage5' )
+let targetImage6  = document.querySelector('#targetImage6' )
+let targetImage7  = document.querySelector('#targetImage7' )
+let targetImage8  = document.querySelector('#targetImage8' )
+let targetImage9  = document.querySelector('#targetImage9' )
+let targetImage10 = document.querySelector('#targetImage10')
+let targetImage11 = document.querySelector('#targetImage11')
+let targetImage12 = document.querySelector('#targetImage12')
+let targetImage13 = document.querySelector('#targetImage13')
+let targetImage14 = document.querySelector('#targetImage14')
+let targetImage15 = document.querySelector('#targetImage15')
+let targetImage16 = document.querySelector('#targetImage16')
+let targetImage17 = document.querySelector('#targetImage17')
+let targetImage18 = document.querySelector('#targetImage18')
+let targetImage19 = document.querySelector('#targetImage19')
+let targetImage20 = document.querySelector('#targetImage20')
+let targetImage21 = document.querySelector('#targetImage21')
+let targetImage22 = document.querySelector('#targetImage22')
+let targetImage23 = document.querySelector('#targetImage23')
+let targetImage24 = document.querySelector('#targetImage24')
+let targetImage25 = document.querySelector('#targetImage25')
+let targetImage26 = document.querySelector('#targetImage26')
+let targetImage27 = document.querySelector('#targetImage27')
+let targetImage28 = document.querySelector('#targetImage28')
+let targetImage29 = document.querySelector('#targetImage29')
+let targetImage30 = document.querySelector('#targetImage30')
+let targetImage31 = document.querySelector('#targetImage31')
+let targetImage32 = document.querySelector('#targetImage32')
+let targetImage33 = document.querySelector('#targetImage33')
+let targetImage34 = document.querySelector('#targetImage34')
+let targetImage35 = document.querySelector('#targetImage35')
+let targetImage36 = document.querySelector('#targetImage36')
+let targetImage37 = document.querySelector('#targetImage37')
+let targetImage38 = document.querySelector('#targetImage38')
+let targetImage39 = document.querySelector('#targetImage39')
+let targetImage40 = document.querySelector('#targetImage40')
+let targetImage41 = document.querySelector('#targetImage41')
+let targetImage42 = document.querySelector('#targetImage42')
+let targetImage43 = document.querySelector('#targetImage43')
+let targetImage44 = document.querySelector('#targetImage44')
+let targetImage45 = document.querySelector('#targetImage45')
+let targetImage46 = document.querySelector('#targetImage46')
+let targetImage47 = document.querySelector('#targetImage47')
+let targetImage48 = document.querySelector('#targetImage48')
+let targetImage49 = document.querySelector('#targetImage49')
+let targetImage50 = document.querySelector('#targetImage50')
+let targetImage51 = document.querySelector('#targetImage51')
+let targetImage52 = document.querySelector('#targetImage52')
+let targetImage53 = document.querySelector('#targetImage53')
+let targetImage54 = document.querySelector('#targetImage54')
+let targetImage55 = document.querySelector('#targetImage55')
+let targetImage56 = document.querySelector('#targetImage56')
+let targetImage57 = document.querySelector('#targetImage57')
+let targetImage58 = document.querySelector('#targetImage58')
+let targetImage59 = document.querySelector('#targetImage59')
+let targetImage60 = document.querySelector('#targetImage60')
+let targetImage61 = document.querySelector('#targetImage61')
+let targetImage62 = document.querySelector('#targetImage62')
+let targetImage63 = document.querySelector('#targetImage63')
+let targetImage64 = document.querySelector('#targetImage64')
+let targetImage65 = document.querySelector('#targetImage65')
+let targetImage66 = document.querySelector('#targetImage66')
+let targetImage67 = document.querySelector('#targetImage67')
+let targetImage68 = document.querySelector('#targetImage68')
+let targetImage69 = document.querySelector('#targetImage69')
+let targetImage70 = document.querySelector('#targetImage70')
+let targetImage71 = document.querySelector('#targetImage71')
+let targetImage72 = document.querySelector('#targetImage72')
+
 
 
 let  equipamentos = [];
@@ -8,27 +79,20 @@ equipamentos.push('Betoneira')
 
 
 function direcionarCamera(){
-  alert(exampleTarget)
-  alert(exampleTarget72)
-  alert(exampleTarget73)
     window.location.href = 'camera.html';
 }
 
-exampleTarget72.addEventListener("targetFound", async event => {
+targetImage72.addEventListener("targetFound", async event => {
+  alert(targetImage72);
   localStorage.setItem('indice', 72)
   window.location.href =  'card.html';
   alert('sou a porra do 72')
   //mandar mensagem ou indice para o local storage e recuperar no outro js
 
 });
-exampleTarget73.addEventListener("targetFound", async event => {
-  window.location.href =  'card.html';
-  alert('sou a porra do 73')
-  //mandar mensagem ou indice para o local storage e recuperar no outro js
 
-});
 
-exampleTarget.addEventListener("targetFound", async event => {
+targetImage.addEventListener("targetFound", async event => {
     window.location.href =  'card.html';
     //mandar mensagem ou indice para o local storage e recuperar no outro js
 
