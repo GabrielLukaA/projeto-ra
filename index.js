5bb93029abc90b3881188b706fdb18f99da64617
unknown <alebatistathiago@gmail.com>
2023-10-07 14:00:58 -0300

    teste

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let targetImages  = document.querySelectorAll("a-entity")
+let targetImages = document.querySelectorAll("a-entity")
 
 function direcionarCamera(){
     window.location.href = 'camera.html';
 }
 
 if(window.location.pathname == '/camera.html'){
-  targetImages.map( (target) => {
+  targetImages.forEach( (target) => {
     target.addEventListener("targetFound", async event => {
       localStorage.setItem('indice', targetImages.indexOf(target))
       window.location.href =  'card.html';
