f41e2745e4c666a6ef035851a0ec5dc5eef251fa
unknown <alebatistathiago@gmail.com>
2023-10-07 14:30:49 -0300

    teste

--- card.js
+++ card.js
@@ -1,12 +1,16 @@
-let text = document.querySelector("#text")
-let title = document.querySelector("#title")
-
+let text = document.querySelector("#text");
+let title = document.querySelector("#title");
 let indice = localStorage.getItem('indice');
+
 alert(indice);
 
 if (indice==72){
     text.innerText = "Sou a imagem 72, represento uma valvula por cima"
     title.innerText = "Válvula"
+}
+
+function direcionarCamera(){
+    window.location.href = 'camera.html';
 }
 
 // alert(text.innerText)
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//Intancia a variável como uma lista
 let targetImages = [];
 
+//Passa para a lista cada elemento "target" da câmera
 document.querySelectorAll("a-entity").forEach( (target) => {
   targetImages.push(target)
 })
@@ -8,14 +10,23 @@ function direcionarCamera(){
     window.location.href = 'camera.html';
 }
 
+//Verifica se está na câmera
 if(window.location.pathname == '/camera.html'){
+  //Passa pela lista de targets adicionando o eventListener
   targetImages.map( (target) => {
     target.addEventListener("targetFound", async event => {
+      //Passa para o localStorage o indice do target
       localStorage.setItem('indice', targetImages.indexOf(target))
       window.location.href =  'card.html';
     });
   });
 }
+
+/////////////////////////////////////////////////////////////////
+//OBS: Não é possivel atribuir os elementos diretamente na 
+//instancia da variável pois eles sao do tipo nodeListOf<Element> 
+//que não possui alguns metodos como o  map por exemplo
+/////////////////////////////////////////////////////////////////
 
   //cpa que fazemos na propria camera.html mesmo e ver se da
     //q nada irmao samo bomd+
