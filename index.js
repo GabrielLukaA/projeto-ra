274c270d83ddef3d2569e6ea001c4511e2c2d6d9
unknown <alebatistathiago@gmail.com>
2023-10-07 12:10:36 -0300

    teste

--- index.js
+++ index.js
@@ -85,8 +85,7 @@ function direcionarCamera(){
 targetImage72.addEventListener("targetFound", async event => {
   alert(targetImage72);
   console.log(targetImage72)
-  console.log(targetImage72.targetIndex)
-  console.log(targetImage72.mindar-image-target)
+  console.log(targetImage72.value)
   localStorage.setItem('indice', 72)
   // window.location.href =  'card.html';
   // alert('sou a porra do 72')
